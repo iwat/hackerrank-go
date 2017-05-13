33babdc9f5752edef9042f998f850ddd90542921
iwatth <iwatth@gmail.com>
2017-05-13 16:07:51 +0700

    Partition linked list

---
+++ linkedlist/partition.go
@@ -0,0 +1,57 @@
+package linkedlist
+
+import (
+	"strings"
+)
+
+func Partition(node *Node, pivot string) *Node {
+	lHead := (*Node)(nil)
+	lTail := (*Node)(nil)
+	eHead := (*Node)(nil)
+	eTail := (*Node)(nil)
+	gHead := (*Node)(nil)
+
+	for node != nil {
+		next := node.Next
+		cmp := strings.Compare(node.Value, pivot)
+		if cmp < 0 {
+			if lTail == nil {
+				lTail = node
+			}
+			node.Next = lHead
+			lHead = node
+		} else if cmp > 0 {
+			node.Next = gHead
+			gHead = node
+		} else {
+			if eTail == nil {
+				eTail = node
+			}
+			node.Next = eHead
+			eHead = node
+		}
+		node = next
+	}
+
+	if lTail != nil {
+		if eHead != nil {
+			lTail.Next = eHead
+		} else if gHead != nil {
+			lTail.Next = gHead
+		}
+	}
+
+	if eTail != nil {
+		if gHead != nil {
+			eTail.Next = gHead
+		}
+	}
+
+	if lHead != nil {
+		return lHead
+	} else if eHead != nil {
+		return eHead
+	} else {
+		return gHead
+	}
+}
---
+++ linkedlist/partition_test.go
@@ -0,0 +1,112 @@
+package linkedlist
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartition(t *testing.T) {
+	head := Create("h", "g", "f", "e", "d", "c", "b", "a")
+	head = Partition(head, "d")
+
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.Equal(t, "d", head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
+
+func TestPartitionExclude(t *testing.T) {
+	head := Create("h", "g", "f", "e", "c", "b", "a")
+	head = Partition(head, "d")
+
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
+
+func TestPartitionLower(t *testing.T) {
+	head := Create("d", "c", "b", "a")
+	head = Partition(head, "d")
+
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.Equal(t, "d", head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
+
+func TestPartitionHigher(t *testing.T) {
+	head := Create("h", "g", "f", "e", "d")
+	head = Partition(head, "d")
+
+	assert.Equal(t, "d", head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
+
+func TestPartitionLowerExclude(t *testing.T) {
+	head := Create("c", "b", "a")
+	head = Partition(head, "d")
+
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") < 0, head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
+
+func TestPartitionHigherExclude(t *testing.T) {
+	head := Create("h", "g", "f", "e")
+	head = Partition(head, "d")
+
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.True(t, strings.Compare(head.Value, "d") > 0, head.Value)
+	head = head.Next
+	assert.Nil(t, head)
+}
