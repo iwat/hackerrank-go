3a29f2e5382b68ba1ea62ce5c4bb2684fae70a9d
iwatth <iwatth@gmail.com>
2017-05-14 16:05:12 +0700

    Weighted mean

---
+++ statistics/weighted_mean.go
@@ -0,0 +1,12 @@
+package main
+
+func WeightedMean(data []int, weight []int) float32 {
+	productSum := 0
+	weightSum := 0
+	for i, item := range data {
+		productSum += item * weight[i]
+		weightSum += weight[i]
+	}
+
+	return float32(productSum) / float32(weightSum)
+}
---
+++ statistics/weighted_mean_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWeightedMean(t *testing.T) {
+	input := []int{10, 40, 30, 50, 20}
+	weight := []int{1, 2, 3, 4, 5}
+	mean := WeightedMean(input, weight)
+	assert.InDelta(t, 32.0, mean, 0.01)
+}
