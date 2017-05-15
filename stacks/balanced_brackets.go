3a000b9307131fa6b9394821bd6f739978207b76
iwatth <iwatth@gmail.com>
2017-05-15 20:52:05 +0700

    Add string backed implementation of balanced brackets

--- stacks/balanced_brackets.go
+++ stacks/balanced_brackets.go
@@ -1,5 +1,26 @@
 package stacks
 
+func BalancedBracketsStringBacked(input string) bool {
+	pair := map[rune]rune{
+		']': '[',
+		'}': '{',
+		')': '(',
+	}
+	stack := make([]rune, 0)
+	for _, c := range input {
+		if open, ok := pair[c]; ok {
+			if len(stack) == 0 || stack[len(stack)-1] != open {
+				return false
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, c)
+		}
+	}
+	return len(stack) == 0
+}
+
 func BalancedBrackets(input string) bool {
 	pair := map[rune]rune{
 		']': '[',
