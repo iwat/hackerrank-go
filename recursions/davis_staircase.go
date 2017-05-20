89ad316b27bb5a4d92371b610384ae6b0fe56f0e
iwatth <iwatth@gmail.com>
2017-05-20 16:41:29 +0700

    Davis' staircase

--- recursions/davis_staircase.go
+++ recursions/davis_staircase.go
@@ -15,3 +15,51 @@ func DavisStaircase(cases int) int {
 	ways += DavisStaircase(cases - 3)
 	return ways
 }
+
+func DavisStaircaseIterative(cases int) int {
+	switch cases {
+	case 1:
+		return 1
+	case 2:
+		return 2
+	case 3:
+		return 4
+	}
+
+	ways := make([]int, cases)
+	ways[0] = 1
+	ways[1] = 2
+	ways[2] = 4
+	for i := 3; i < cases; i++ {
+		ways[i] = ways[i-1] + ways[i-2] + ways[i-3]
+	}
+	return ways[len(ways)-1]
+}
+
+func DavisStaircaseIterativeSlow(cases int) int {
+	ways := 0
+	queue := []int{cases}
+	for len(queue) > 0 {
+		n := queue[0]
+		queue = queue[1:]
+
+		switch n {
+		case 0:
+			break
+		case 1:
+			ways += 1
+			break
+		case 2:
+			ways += 2
+			break
+		case 3:
+			ways += 4
+			break
+		default:
+			queue = append(queue, n-1)
+			queue = append(queue, n-2)
+			queue = append(queue, n-3)
+		}
+	}
+	return ways
+}
--- recursions/davis_staircase_test.go
+++ recursions/davis_staircase_test.go
@@ -13,3 +13,19 @@ func TestDavisStaircase(t *testing.T) {
 
 	assert.Equal(t, 2082876103, DavisStaircase(36))
 }
+
+func TestDavisStaircaseIterative(t *testing.T) {
+	assert.Equal(t, 1, DavisStaircaseIterative(1))
+	assert.Equal(t, 4, DavisStaircaseIterative(3))
+	assert.Equal(t, 44, DavisStaircaseIterative(7))
+
+	assert.Equal(t, 2082876103, DavisStaircaseIterative(36))
+}
+
+func TestDavisStaircaseIterativeSlow(t *testing.T) {
+	assert.Equal(t, 1, DavisStaircaseIterative(1))
+	assert.Equal(t, 4, DavisStaircaseIterative(3))
+	assert.Equal(t, 44, DavisStaircaseIterative(7))
+
+	assert.Equal(t, 2082876103, DavisStaircaseIterative(36))
+}
