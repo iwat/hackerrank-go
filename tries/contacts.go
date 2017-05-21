3a4232e9ad417dc90583cdc61e4381567dee39c1
iwatth <iwatth@gmail.com>
2017-05-21 17:11:59 +0700

    Tries: Contact, optimized

--- tries/contacts.go
+++ tries/contacts.go
@@ -3,19 +3,22 @@ package tries
 type Trie struct {
 	value    string
 	children map[byte]*Trie
+	count    int
 	final    bool
 }
 
 func NewTrie(value string) *Trie {
-	return &Trie{value, make(map[byte]*Trie), false}
+	return &Trie{value, make(map[byte]*Trie), 0, false}
 }
 
 func (t *Trie) Add(input string) {
 	c := input[0]
 	if child, ok := t.children[c]; ok {
+		child.count++
 		child.Add(input[1:])
 	} else {
 		t.children[c] = NewTrie(t.value + input[0:1])
+		t.children[c].count = 1
 		if len(input) > 1 {
 			t.children[c].Add(input[1:])
 		} else {
@@ -26,7 +29,7 @@ func (t *Trie) Add(input string) {
 
 func (t *Trie) Find(input string) int {
 	if len(input) == 0 {
-		return t.Count()
+		return t.count
 	}
 
 	if child, ok := t.children[input[0]]; ok {
@@ -34,17 +37,4 @@ func (t *Trie) Find(input string) int {
 	} else {
 		return 0
 	}
-}
-
-func (t *Trie) Count() int {
-	count := 0
-	for _, child := range t.children {
-		count += child.Count()
-	}
-
-	if t.final {
-		count++
-	}
-
-	return count
 }
