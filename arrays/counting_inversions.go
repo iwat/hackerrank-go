4bc233c865f0f16e7f5d5e8ddc446e7dc631ac99
iwatth <iwatth@gmail.com>
2017-05-23 20:29:11 +0700

    Copy aux memory once

--- arrays/counting_inversions.go
+++ arrays/counting_inversions.go
@@ -5,41 +5,41 @@ package arrays
 // https://www.hackerrank.com/challenges/ctci-merge-sort
 func CountingInversions(data []int) int {
 	aux := make([]int, len(data))
+	copy(aux, data)
 	return mergeSort(data, aux)
 }
 
-func mergeSort(data, aux []int) int {
-	if len(data) <= 1 {
+func mergeSort(dest, src []int) int {
+	if len(dest) <= 1 {
 		return 0
 	}
-	swaps := mergeSort(data[:len(data)/2], aux)
-	swaps += mergeSort(data[len(data)/2:], aux)
-	swaps += merge(data, aux)
+	swaps := mergeSort(src[:len(dest)/2], dest[:len(src)/2])
+	swaps += mergeSort(src[len(dest)/2:], dest[len(src)/2:])
+	swaps += merge(dest, src)
 	return swaps
 }
 
-func merge(data, aux []int) int {
+func merge(dest, src []int) int {
 	swaps := 0
 	i := 0
-	left := data[:len(data)/2]
-	right := data[len(data)/2:]
+	left := src[:len(src)/2]
+	right := src[len(src)/2:]
 	for len(left) > 0 || len(right) > 0 {
 		if len(left) == 0 {
-			aux[i] = right[0]
+			dest[i] = right[0]
 			right = right[1:]
 		} else if len(right) == 0 {
-			aux[i] = left[0]
+			dest[i] = left[0]
 			left = left[1:]
 		} else if left[0] <= right[0] {
-			aux[i] = left[0]
+			dest[i] = left[0]
 			left = left[1:]
 		} else {
-			aux[i] = right[0]
+			dest[i] = right[0]
 			right = right[1:]
 			swaps += len(left)
 		}
 		i++
 	}
-	copy(data, aux)
 	return swaps
 }
